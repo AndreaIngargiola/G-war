e8d64faf9e7d19f6b03e4ca4d3df2c7a2d6753e4
ELISA GAMBARETTO <elisa.gambaretto@studio.unibo.it>
2019-05-22 14:23:01 +0200

    create the interface for control the screen

---
+++ src/view/ScreenFX.java
@@ -0,0 +1,15 @@
+package view;
+
+import javafx.scene.Node;
+
+/**
+ * Control the screen.
+ */
+public interface ScreenFX {
+
+    /**
+     * @return the node of screen
+     */
+    Node getNode();
+
+}

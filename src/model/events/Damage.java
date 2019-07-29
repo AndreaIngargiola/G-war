ab95a5ff60358fa0b62a652b4b21e4fbecfb24c4
VERONICA BIANCACCI <veronica.biancacci@studio.unibo.it>
2019-07-29 22:20:43 +0200

    create the package event with EntityEvent, AbstractEntityEvent, EntityEventSubscriber and Damage classes

---
+++ src/model/events/Damage.java
@@ -0,0 +1,34 @@
+package model.events;
+
+import model.entities.Entity;
+
+/**
+ * Models an event Damage (when an entity loses health points).
+ *
+ */
+
+public class Damage extends AbstractEntityEvent {
+
+    private final int loss;
+
+    /**
+     * 
+     * @param source
+     *           the entity that loses health points
+     * @param loss
+     *           the number of health points that the source loses 
+     */
+    public Damage(final Entity source, final int  loss) {
+        super(source);
+        this.loss = loss;
+    }
+
+    /**
+     * 
+     * @return the number of health points that the source loses
+     */
+    public int getLoss() {
+        return this.loss;
+    }
+
+}

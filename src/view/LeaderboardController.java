b300e722fd502cd7537377e43587c8577928b21c
ELISA GAMBARETTO <elisa.gambaretto@studio.unibo.it>
2019-08-06 16:35:45 +0200

    modify the class LeaderboardController for add the information of player in Leaderboard view

--- src/view/LeaderboardController.java
+++ src/view/LeaderboardController.java
@@ -1,5 +1,23 @@
 package view;
 
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -27,20 +45,60 @@ public class LeaderboardController {
      * @return
      *         the list of player.
      */
-    public ObservableList<PlayerLeaderboard> getPlayerList() {
+    /*public ObservableList<PlayerLeaderboard> getPlayerList() {
         ObservableList<PlayerLeaderboard> playerList = FXCollections.observableArrayList();
         playerList.add(new PlayerLeaderboard("E", 5000));
         playerList.add(new PlayerLeaderboard("A", 3000));
         playerList.add(new PlayerLeaderboard("V", 2000));
         return playerList;
-    }
+    }*/
 
     @FXML
     private void initialize() {
         playerNameColumn.setCellValueFactory(cellData -> cellData.getValue().playerNameProperty());
         playerScoreColumn.setCellValueFactory(cellData -> cellData.getValue().playerScoreProperty().asObject());
-        playerTable.setItems(this.getPlayerList());
+        ObservableList<PlayerLeaderboard> playerList = FXCollections.observableArrayList();
+        playerTable.setItems(playerList);
 
+
+        try {
+            File inputFile = new File("src/view/CharacterScores.xml"); 
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            Document doc = docBuilder.parse(inputFile);
+            //Node characters = doc.getFirstChild();
+
+            doc.getDocumentElement().normalize();
+
+            //System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            NodeList nList = doc.getElementsByTagName("character");  //Lista di character (ora sono tre) con i loro nodi figli
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);  //scorre uno per uno i nodi character
+                //System.out.println("\nCurrent Element :" + nNode.getNodeName());
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    playerList.add(new PlayerLeaderboard(eElement.getElementsByTagName("player").item(0).getTextContent(), Integer.parseInt(eElement.getElementsByTagName("score").item(0).getTextContent())));
+                }
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(inputFile);
+            transformer.transform(source, result);
+
+            System.out.println("Add the information of player in the TableView of LoaderBoard");
+
+        } catch (ParserConfigurationException pce) {
+            pce.printStackTrace();
+        } catch (TransformerException tfe) {
+            tfe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (SAXException sae) {
+            sae.printStackTrace();
+        }
     }
 
 

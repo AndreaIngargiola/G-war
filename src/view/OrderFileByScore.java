3c3ad1db10e7c0cb885386b3d3a781a433d9f14f
ELISA GAMBARETTO <elisa.gambaretto@studio.unibo.it>
2019-08-06 16:37:04 +0200

    modify the class OrderFileByScore for order the information of player by score

--- src/view/OrderFileByScore.java
+++ src/view/OrderFileByScore.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;

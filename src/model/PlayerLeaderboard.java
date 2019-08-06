c36bd0c739ae0c0e70b38c9ba131fe58e69c36ee
ELISA GAMBARETTO <elisa.gambaretto@studio.unibo.it>
2019-08-06 10:10:54 +0200

    create new package model and the class PlayerLoaderboard for keep information about player in loaderboard

---
+++ src/model/PlayerLeaderboard.java
@@ -0,0 +1,52 @@
+package model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+/**
+ * Model class for a player in loaderboard.
+ */
+public class PlayerLeaderboard {
+    private final StringProperty playerName;
+    private final IntegerProperty playerScore;
+
+    /**
+     * Default constructor.
+     */
+    public PlayerLeaderboard() {
+        this(null, null);
+    }
+
+    /**
+     * Constructor with some initial data.
+     * 
+     * @param playerName
+     *         the name of player.
+     * @param playerScore
+     *         the score of player.
+     */
+    public PlayerLeaderboard(final String playerName, final Integer playerScore) {
+        this.playerName = new SimpleStringProperty(playerName);
+        this.playerScore = new SimpleIntegerProperty(playerScore);
+    }
+
+    /**
+     * method for get the name of player.
+     * @return
+     *         the name of score.
+     */
+    public StringProperty playerNameProperty() {
+        return playerName;
+    }
+
+    /**
+     * method for get the score of player.
+     * @return
+     *         the name of score.
+     */
+    public IntegerProperty playerScoreProperty() {
+        return playerScore;
+    }
+
+}

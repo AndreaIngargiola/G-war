52911e0c07181264357dde182ad79e7521ac0c6e
ELISA GAMBARETTO <elisa.gambaretto@studio.unibo.it>
2019-08-08 17:15:04 +0200

    Create interface of controller of view

---
+++ src/view/ControllerView.java
@@ -0,0 +1,14 @@
+package view;
+/**
+ * Interface for controller of view.
+ */
+public interface ControllerView {
+
+    /**
+     * Sets main menu.
+     * @param mainMenu
+     *         the main menu.
+     */
+    void setMainMenuGame(MainMenuGame mainMenu);
+
+}

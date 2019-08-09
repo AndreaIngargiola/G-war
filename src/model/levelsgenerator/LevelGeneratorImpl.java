cda4da30878d8b435908a0815e940f5491fb3817
Andrea Ingargiola <andrea.ingargiola@studio.unibo.it>
2019-08-09 11:46:08 +0200

    develop further the LevelGenerator class (create fields and blocks initilization), add IncompatibleWorkingSet exception

--- src/model/levelsgenerator/LevelGeneratorImpl.java
+++ src/model/levelsgenerator/LevelGeneratorImpl.java
@@ -2,6 +2,7 @@ package model.levelsgenerator;
 
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,17 +10,32 @@ import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ClassInfoList;
 import io.github.classgraph.ScanResult;
 import model.entities.AbstractEntity;
+import model.entities.Player;
 import model.levelsgenerator.conditions.ConditionGiver;
 import model.levelsgenerator.conditions.ConditionGiverImpl;
+import model.levelsgenerator.geometry.Coordinate;
+import model.levelsgenerator.geometry.Grid;
+import model.levelsgenerator.geometry.GridImpl;
+import model.math.BallsUrn;
+import model.math.BallsUrnImpl;
 
 /**
  * An implementation of LevelGenerator interface that uses advanced reflection functions (via ClassGraph library) and modular classes.
  */
 public class LevelGeneratorImpl implements LevelGenerator {
 
+    private static final int BALLS_NUMBER = 10;
+    private static final int GRID_RANK = 10;
+    private static final Coordinate JUMP_RANGE = new Coordinate(3, 3);
+    
+    private LevelGenerationEntity<Player> player;
+    private LevelGenerationEntity<Floor> floor;
+
     private List<LevelGenerationEntity<? extends AbstractEntity>> entityList;
-    private List<EntityBlock> blockList;
+    private Map<EntityBlock, BallsUrn> blockMap;
+    private Map<EntityBlock, BallsUrn> architecture;
     private ConditionGiver cg;
+    private Grid levelGrid;
     private Integer iteration;
 
     /**
@@ -42,7 +58,7 @@ public class LevelGeneratorImpl implements LevelGenerator {
      * Create the entity set scanning the whole build path, searching for classes that extends AbstractEntity and doesn't possess derived classes.
      * The non-definitive class exclusion is essential for granting that only the final version of an entity will be added to the active rotation spawn list.
      * If in future someone want to edit an existing enemy expanding the enemy class but keep the current version of the same enemy too, he has to extend the enemy class in two way:
-     * the edited one and a "fake" one that changes nothing for preserving the current state of the entity.   
+     * the edited one and a "fake" one that changes nothing for preserving the current state of the entity.
      */
     private void importEntities() {
         try (ScanResult scanResult = new ClassGraph().enableAllInfo().scan()) {
@@ -57,6 +73,18 @@ public class LevelGeneratorImpl implements LevelGenerator {
                     .map(c -> c.loadClass(type))
                     .forEach(e -> entityList.add(new LevelGenerationEntity<>(type.cast(e))));
         }
+
+        /*if there is no player or at least one architecture entities throw exception*/
+        if (this.entityList.stream()
+                           .map(e -> e.getEntityName())
+                           .noneMatch(en -> en.equals("Player")) 
+                           || 
+            this.entityList.stream()
+                           .map(e -> e.getComponentsSet())
+                           .filter(cs -> cs.contains("Architecture"))
+                           .count() == 0) {
+            throw new IncompatibleEntitiesWorkingSet();
+        }
     }
 
     /**
@@ -70,7 +98,7 @@ public class LevelGeneratorImpl implements LevelGenerator {
                     classInfo -> (classInfo.implementsInterface("ConditionGiver") 
                                && !classInfo.hasAnnotation("DefaultConditionGiver")));
             if (filtered.isEmpty()) {
-                new ConditionGiverImpl();
+                this.cg = new ConditionGiverImpl();
             } else {
                 filtered.stream()
                         .sorted((c1, c2) -> c1.getSimpleName().compareTo(c2.getSimpleName()))
@@ -80,10 +108,28 @@ public class LevelGeneratorImpl implements LevelGenerator {
     }
 
     private void initializeBlocksMap() {
-        final List<EntityBlock> blockList = new ArrayList<>();
-        for (LevelGenerationEntity<?> entity : this.entityList) {
-            blockList.add(new EntityBlock(entity, this.cg));
+
+        this.blockMap = new HashMap<>();
+        this.architecture = new HashMap<>();
+
+        for (LevelGenerationEntity<?> e : this.entityList) {
+            if (e.getComponentsSet().contains("Architecture")) {
+                this.architecture.put(new EntityBlock(e, this.cg), new BallsUrnImpl(LevelGeneratorImpl.BALLS_NUMBER));
+            } else {
+                this.blockMap.put(new EntityBlock(e, this.cg), new BallsUrnImpl(LevelGeneratorImpl.BALLS_NUMBER));
+            }
         }
+    }
+
+    private void architectureBuilder() {
+        for (int i = 0; i < LevelGeneratorImpl.GRID_RANK; i++) {
+            this.levelGrid.place(new Coordinate(i, 0), new EntityBlock(this.floor));
+        }
+        
+    }
+
+    private void firstIteration() {
+        this.levelGrid = new GridImpl(LevelGeneratorImpl.GRID_RANK);
 
     }
 }

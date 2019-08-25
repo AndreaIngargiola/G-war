8ebd52bc88f75b95f09c313d2ff0706b6729b37f
Andrea Ingargiola <andrea.ingargiola@studio.unibo.it>
2019-08-25 16:44:09 +0200

    add javadoc in Invisible Wall entity

--- src/model/entities/InvisibleWall.java
+++ src/model/entities/InvisibleWall.java
@@ -5,14 +5,15 @@ import org.jbox2d.common.Vec2;
 import enumerators.Faction;
 import model.physics.BodyBuilder;
 
+/**
+ * An invisible wall for forbid out of bounds positions to the player.
+ * This entity will be placed directly in the game model and not via level generator because is not a gameplay-wise entity.
+ */
 @LevelGenerationIgnore
 public final class InvisibleWall extends AbstractEntity {
 
     private static final Faction TYPE = Faction.NEUTRAL_IMMORTAL;
     private static final Vec2 SIZE = new Vec2(10, 400);
-    /**
-     * Used for the importation of the entity by reflection.
-     */
 
     /**
      * 

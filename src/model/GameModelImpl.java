ea77497d35be8719dd9366de7046dbf00dd58b17
Andrea Ingargiola <andrea.ingargiola@studio.unibo.it>
2019-08-25 16:44:48 +0200

    final version of GameModel interface and implementation

--- src/model/GameModelImpl.java
+++ src/model/GameModelImpl.java
@@ -11,30 +11,31 @@ import java.util.Timer;
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.World;
 
-import com.sun.javafx.geom.Vec2d;
-
 import controller.EntityController;
 import controller.EntityFactory;
 import controller.EntityFactoryImpl;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
 import javafx.util.Duration;
+
+import model.components.Points;
 import model.engine.TimerTaskGrill;
-import model.entities.Entity;
 import model.entities.InvisibleWall;
 import model.levelsgenerator.LevelGenerator;
 import model.levelsgenerator.LevelGeneratorImpl;
 import model.physics.BodyBuilder;
 import model.physics.BodyBuilderImpl;
 import model.physics.MyContactListener;
+
 import view.GameViewImpl;
-import view.StatisticsView;
 
-
-public class GameModelImpl implements GameModel{
+/**
+ * An implementation of the game loop model that uses a dynamic creation of levels via LevelGeneratorImpl and EntityFactoryImpl.
+ */
+public class GameModelImpl implements GameModel {
 
     /**
      * ENTITIES is the list of active entities in the game engine that will be updated each frame.
@@ -43,17 +44,20 @@ public class GameModelImpl implements GameModel{
 
     private static final Vec2 GRAVITY =  new Vec2(0, 60);
     private static final World MYWORLD = new World(GRAVITY, true);
+    private static final int LEVEL_EXTENSION = 200;
     private static final double FRAMERATE = 1.0 / 60;
     private static final TimerTaskGrill TIMER_TASK = new TimerTaskGrill();
     private static final int ENTITIES_PIXEL_DIMENSION = 10;
+    private static final int POINTS = 50;
+    private static final float TIMESTEP = 0.017f;
+    private static final int VELOCITY_ITERATION = 5;
+    private static final int POSITION_ITERATION = 2;
+    private static final Timer TIMER = new Timer();
+    private static final Vec2 INVISIBLE_WALL_DIM = new Vec2(10, 400);
+    private static final int LEVELS_IN_STACK = 3;
+    private static final int TIMER_TIME = 3000;
 
-    private final Timer timer = new Timer();
-    private final float timestep = 0.017f;
-    private final int velocityIteration = 5;
-    private final int positionIteration = 6;
     private final Timeline gameLoop = new Timeline();
-
-    private final int levelExtension = 200;
     private final LevelGenerator lg;
     private final EntityFactory entityFactory = new EntityFactoryImpl(GameViewImpl.getRoot());
     private EntityController player;
@@ -63,48 +67,67 @@ public class GameModelImpl implements GameModel{
                 Duration.seconds(GameModelImpl.FRAMERATE),                // 60 FPS
                 new EventHandler<ActionEvent>() {
                     public void handle(final ActionEvent ae) {
-                        MYWORLD.step(timestep, velocityIteration, positionIteration);
+                        MYWORLD.step(GameModelImpl.TIMESTEP, 
+                                     GameModelImpl.VELOCITY_ITERATION, 
+                                     GameModelImpl.POSITION_ITERATION);
                     }
                 }
 );
 
-    public  GameModelImpl () {
-            timer.schedule(TIMER_TASK, 0, 3000);
-
+    /**
+     * An implementation for the game model that uses this.
+     */
+    public  GameModelImpl() {
+            GameModelImpl.TIMER.schedule(TIMER_TASK, 0, GameModelImpl.TIMER_TIME);
             this.lg = new LevelGeneratorImpl();
-
             MYWORLD.setContactListener(new MyContactListener());
-
             gameLoop.setCycleCount(Timeline.INDEFINITE);
             GameModelImpl.ENTITIES.add(new ArrayList<>());
-
-            BodyBuilder bodyWall = new BodyBuilderImpl();
-            new InvisibleWall(bodyWall, new Vec2(-40, 200));
-
             gameLoop.getKeyFrames().add(kf);
-            gameLoop.play();
-            
-            try {
-                this.generateLevel();
-            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) { 
-                e.printStackTrace();
-            }
-            
-            //a little override of previous position that prevents delays in the level spawn.
-            this.previousPosition = 0;
+    }
+
+    @Override
+    public final void start() {
+        final BodyBuilder bodyWall = new BodyBuilderImpl();
+        new InvisibleWall(bodyWall, new Vec2(-GameModelImpl.INVISIBLE_WALL_DIM.x, GameModelImpl.INVISIBLE_WALL_DIM.y / 2));
+
+        gameLoop.play();
+        try {
+            this.generateLevel();
+            this.generateLevel();
+        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) { 
+            e.printStackTrace();
+        }
+
+        //a little override of previous position that prevents delays in the level spawn.
+        this.previousPosition = 0;
+    }
+ 
+    @Override
+    public final void stop() {
+        gameLoop.stop();
+            reset();
+    }
+
+    private void reset() {
+        GameModelImpl.ENTITIES.stream()
+        .forEach(e -> e.stream().forEach(i -> i.getEntityModel().destroy()));
+        GameModelImpl.ENTITIES.clear();
+        GameViewImpl.getStatistics().setMaxHealth();
     }
 
     private void generateLevel() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 
         //add the iteration-th array to ENTITIES and update the offset.
         GameModelImpl.ENTITIES.add(new ArrayList<>());
-        float offset = this.levelExtension * lg.getIteration();
+        final float offset = GameModelImpl.LEVEL_EXTENSION * lg.getIteration();
 
         //get a new level from the level generator and process the output
         final Map<Point, String> levelDraft = this.lg.getNewLevel();
 
-        for (Point p : levelDraft.keySet()) { 
-            final Vec2 convertedCoordinate = new Vec2((p.x * 10) + offset, -(p.y * 10) + 150);
+        for (final Point p : levelDraft.keySet()) { 
+            final Vec2 convertedCoordinate = new Vec2((p.x * GameModelImpl.ENTITIES_PIXEL_DIMENSION) + offset, 
+                                                     -(p.y * GameModelImpl.ENTITIES_PIXEL_DIMENSION) + 150);
                 Method m;
                 try {
                     if (levelDraft.get(p).equals("Player")) {
@@ -126,44 +149,66 @@ public class GameModelImpl implements GameModel{
         this.previousPosition = this.player.getEntityModel().getBody().getPosition().x;
     }
 
+    /**
+     * Get a snapshot of the game world.
+     * @return a snapshot of the current situation in the Physics World.
+     */
     public static World getWorld() {
                 return MYWORLD;
         }
 
+    /**
+     * Get the state of the grills timer.
+     * @return the TimerTask that manages grills.
+     */
     public static TimerTaskGrill getTimer() {
                 return TIMER_TASK;
         }
 
+    /**
+     * Get the current active entities.
+     * @return all the entities in the list.
+     */
     public static List<List<EntityController>> getEntities() {
         return ENTITIES;
     }
 
     @Override
-    public void  updateEntity(final double dt) {
-        ENTITIES.stream()
-                .forEach(l -> {
-                    l.stream().forEach(e -> e.update(dt));
-                });
-        this.player.update(dt);
+    public final boolean updateEntity(final double dt) {
 
-        if (this.player.getEntityModel().getBody().getPosition().x - this.previousPosition > this.levelExtension) {
-            try {
-                this.generateLevel();
-            } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+        //check if the player is alive
+        if (!this.player.getEntityModel().isAlive()) {
+            stop(); 
+        } else {
+
+            //update all entities.
+            GameModelImpl.ENTITIES.stream()
+                                  .forEach(l -> l.stream().forEach(e -> e.update(dt)));
+            this.player.update(dt);
+
+            //check if another level must be created
+            if (this.player.getEntityModel().getBody().getPosition().x - this.previousPosition > GameModelImpl.LEVEL_EXTENSION) {
+                try {
+                    this.generateLevel();
+                    this.player.getEntityModel().get(Points.class).addPoints(POINTS);
+                } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+
+                //check if the past levels must be erased.
+                if (this.lg.getIteration() > GameModelImpl.LEVELS_IN_STACK) {
+                    final int desideredLevelNumber = this.lg.getIteration() - (GameModelImpl.LEVELS_IN_STACK + 1);
+
+                    GameModelImpl.ENTITIES.get(desideredLevelNumber).stream().forEach(e -> e.getEntityModel().destroy());
+                    GameModelImpl.ENTITIES.get(desideredLevelNumber).clear();
+
+                    //place an invisible wall so the player cannot reach deallocated areas.
+                    BodyBuilder bodyWall = new BodyBuilderImpl();
+                    new InvisibleWall(bodyWall, new Vec2((desideredLevelNumber + 1) * GameModelImpl.LEVEL_EXTENSION, GameModelImpl.LEVEL_EXTENSION));
+                }
             }
-
-            if (this.lg.getIteration() > 3) {
-                final int desideredLevelNumber = this.lg.getIteration() - 4;
-
-                System.out.println("deallocate lvl: " + desideredLevelNumber);
-                GameModelImpl.ENTITIES.get(desideredLevelNumber).stream().forEach(e -> e.getEntityModel().destroy());
-                GameModelImpl.ENTITIES.get(desideredLevelNumber).clear();
-                BodyBuilder bodyWall = new BodyBuilderImpl();
-                new InvisibleWall(bodyWall, new Vec2((desideredLevelNumber + 1) * this.levelExtension, this.levelExtension));
-            }
+            return false;
         }
+        return true;
     }
-
 }
